b92532c02bdb69deb9d5a84256bd27176d0eec95
MJ Ogama <mj@MJs-MacBook-Pro.local>
2026-06-15 01:05:03 +0800

    Initial push

---
+++ server/dist/server.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+const PORT = Number(process.env.PORT) || 8080;
+const serverStarter = async () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+serverStarter();
+//# sourceMappingURL=server.js.map
